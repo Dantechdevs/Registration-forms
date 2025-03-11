4b83d522428e015d6f5556bbc406410f7e46fe94
dantechdevs <damnngwasi@gmail.com>
2025-03-11 15:22:16 +0300

    updated

--- registration/registration/views.py
+++ registration/registration/views.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def homepage(request):
-    return HttpResponse("Hello, World!',im at Nairobi")
-def about(request):
-    return HttpResponse("This is the about page")
+# def homepage(request):
+    # return HttpResponse("Hello, World!',im at Nairobi")
+# def about(request):
+    # return HttpResponse("This is the about page")
+    from django.shortcuts import render 
+    
+    def homepage(request):
+        return render(request,'home.html')
+    def about(request):
+        return render(request,'about.html')                 
+#
     
